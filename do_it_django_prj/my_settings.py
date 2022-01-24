c6c4196817a5cba5913c09e8b7946c0b2150584d
yebun91 <65695703+yebun91@users.noreply.github.com>
2022-01-24 14:16:02 +0900

    다시 작업중..

---
+++ do_it_django_prj/my_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_web',
+        'USER': 'root',
+        'PASSWORD': '7895',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
